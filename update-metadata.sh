0974d85055c2ea58d59195274158f159110ff3d6
Chris Fordham <chris.fordham@rightscale.com>
2012-08-03 14:17:39 +1000

    Add update-metadata.sh.

---
+++ update-metadata.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+# generate metadata.json for upstream opscode cookbooks that have no metadata.json
+knife cookbook metadata apache2 -o cookbooks/
+knife cookbook metadata cron -o cookbooks/
+knife cookbook metadata ntp -o cookbooks/
+#knife cookbook metadata postfix -o cookbooks/
+knife cookbook metadata python -o cookbooks/
+knife cookbook metadata sudo -o cookbooks/
+#knife cookbook metadata collectd -o cookbooks/
